f4a4fbb47eb6554d1dd1731ff024bcf1ff525047
Abhi Kumar <abhi_yallapu@tecnics.com>
2024-11-20 10:10:25 +0530

    Add types for Playlist

---
+++ src/graphql/types/types.playlist.ts
@@ -0,0 +1,39 @@
+import { JsonValue } from "@prisma/client/runtime/library";
+
+import {Media} from './types.media';
+
+export type Playlist = {
+    playlistId: string;
+    title: string;
+    type: string;
+    playlist: Media[];
+    description?: string;
+    customParameters: JsonValue[];
+    kind: string;
+    createdAt: Date;
+    updatedAt: Date;
+};
+
+export type PlaylistMetadata = {
+    title: string;
+    description: string;
+    type: string,
+    customParameters?: string,
+}
+
+export type DynamicPlaylistConfig = {
+    tags?: {
+        include?: string;
+        exclude?: string;
+    };
+    customParameters?: {
+        include?: { [key: string]: any };
+        exclude?: { [key: string]: any };
+    };
+    pageNumber?: number;
+    itemsPerPage?: number;
+    sort?: {
+        field?: string;
+        order: "asc" | "desc";
+    };
+};
