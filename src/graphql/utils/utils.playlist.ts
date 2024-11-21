774be8cdd7fd6616b1b1597d48391ac9c83e210a
Abhi Kumar <abhi_yallapu@tecnics.com>
2024-11-21 13:13:19 +0530

    Refactor playlist formatting to use spread operator for customParameters
    - Simplified the integration of customParameters into the playlist structure using the spread operator. - Enhanced code clarity and maintainability by eliminating the use of Object.entries loop.

--- src/graphql/utils/utils.playlist.ts
+++ src/graphql/utils/utils.playlist.ts
@@ -1,18 +1,12 @@
 export function formatPlaylist(rawPlaylist: any): { [key: string]: any } {
     const { playlistId, title, type, description, customParameters, playlist } = rawPlaylist;
-    const formattedPlaylist: { [key: string]: any } = {
+
+    return {
         title,
         feedid: playlistId,
         description,
         kind: type,
         playlist,
+        ...(customParameters || {}), // Spread customParameters if it exists
     };
-
-    if (customParameters) {
-        Object.entries(customParameters).forEach(([key, value]) => {
-            formattedPlaylist[key] = value;
-        });
-    }
-
-    return formattedPlaylist;
 }
